215525981076a3932699e1907da0ba5b8493c494
Nenye Diei <nenyediei@gmail.com>
2021-05-25 10:33:04 -0500

    initial database push

--- final-capstone/java/database/schema.sql
+++ final-capstone/java/database/schema.sql
@@ -1,9 +1,18 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS breweries; 
+
 DROP SEQUENCE IF EXISTS seq_user_id;
+DROP SEQUENCE IF EXISTS seq_brewery_id;
 
 CREATE SEQUENCE seq_user_id
+  INCREMENT BY 1
+  NO MAXVALUE
+  NO MINVALUE
+  CACHE 1;
+  
+  CREATE SEQUENCE seq_brewery_id
   INCREMENT BY 1
   NO MAXVALUE
   NO MINVALUE
@@ -15,11 +24,27 @@ CREATE TABLE users (
 	username varchar(50) NOT NULL,
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
+        account_type varchar(25) NOT NULL,
+        user_location varchar(35),  
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
-INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
-INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+CREATE TABLE breweries (
+	brewery_id int DEFAULT nextval('seq_brewery_id'::regclass) NOT NULL,
+	brewery_name varchar(50) NOT NULL,
+        brewer_id int NOT NULL,
+        brewery_street_address varchar,
+        brewery_city varchar,
+        brewery_state varchar,
+        brewery_zip int,   
+	CONSTRAINT PK_brewery PRIMARY KEY (brewery_id),
+	CONSTRAINT FK_brewer_id FOREIGN KEY(brewer_id) REFERENCES users(user_id)
+);
 
+
+INSERT INTO users (username,password_hash,role, account_type, user_location) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER', 'Beer Lover', 'Ohio');
+INSERT INTO users (username,password_hash,role, account_type, user_location) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN', 'Administrator', 'Texas');
+INSERT INTO users (username,password_hash,role, account_type, user_location) VALUES ('brewer','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN', 'Brewer', 'Wisconsin');
 
 COMMIT TRANSACTION;
+
