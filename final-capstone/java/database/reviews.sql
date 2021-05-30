95a0d8b06c1fe51aa9f1ff4da8764577666186c1
seror salman <serorsalman@gmail.com>
2021-05-30 12:05:22 -0500

    changed DB reviews

--- final-capstone/java/database/reviews.sql
+++ final-capstone/java/database/reviews.sql
@@ -1,4 +1,46 @@
 -- ************************************************************************************
 -- This script populates the reviews table with sample data (beer_id must exist)
 -- ************************************************************************************
-INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (11, 2, 'Tasty beer, full of flavors. I enjoyed it!', 4);
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (1, 2, 'Tasty beer, full of flavors. I enjoyed it!', 5);
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (1, 2, 'Totally works for me a lot of Southern Tier Blackwater beers are in my sweet tooth wheelhouse & this is another shining example; great drinkability as well', 4.44);
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (1, 2, 'An incredible beer/stout and a very nice aperitivo. It tastes like superior ingredients put into a Duncan Hines German chocolate cake mix with coconut caramel icing.', 4.38);
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (1, 2, 'Overall I will without a doubt I will be getting more of this, really nice stuff and like always with Southern Tier something you won’t forget having. ', 4.25);
+
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (2, 2, 'A sweet milk stout with all the malt characteristics you love but with a bold, unique twist- this beer tastes like a glass full of blueberry pancakes smothered in maple syrup.', 3.76);
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (2, 2, 'Loved this stout. Very dark. Poured from a can. Small head. Smelled like chocolate, blueberry, maple. Just as advertised. The blueberry came through in the taste. Not over powering. Just enough.', 4.35);
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (2, 2, 'These beers can be difficult without a proper alcohol content for warming and backboning. My fault for wanting something more properly stout (and Saugatuck does produce those), but this was more like a sugary soft drink than a beer.', 2.67);
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (2, 2, 'Poured from a can. Pours dark with a very thin tan head. Smell of roasted grain, blueberry and chocolate. Flavor follows the smell. Blueberry flavor is good and feels natural, get some maple flavor in the finish. Interesting.', 3.6);
+
+
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (3, 3, 'What a great beer. Pours, not too hazy, but just right. Nose is juicy. Head and lacing are beautiful. Ever so slight powdery feeling that I always seems to get with the haxy’s.', 4.3);
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (3, 3, 'An opaque but glowing light lemon body color, with a supper rich creamy filling bright white head of three fingers. Very fine bubbles in the head, with ring lacing, and a creamy island retention volcano. Settles real slowly looking like a great IPA.', 4.14);
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (3, 3, 'Funny how a nose can misdirect. The nose is a bit acrid and funky but the taste is luscious, fruity and mildly hoppy with plenty of ripe tropical taste in a great package of taste.', 4.14);
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (3, 3, 'Overall: A lovely pale ale when youre in the mood for some FRUIT', 4.18);
+
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (4, 4, ' I do taste watermelon, and a simple, non-descript wheat ale. Its a watermelon wheat, a decent one, best used to rev your engine or quench your thirst.', 3.38);
+
+
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (5, 5, "Great new IPA, I enjoyed the taste, body and appearance. Tends to be a little dry, very similar to Fiddlehead. I've always enjoyed Sierra Nevada Pale Ale, and Hazy Little Thing is a nice complement.", 4.6);
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (5, 5, "This is just unwavering and completely solid, I'm not mind blown but perfectly content with the decision, this is my first beer from SN actually so now im looking more forward to the classic pale ale and the other products.", 4);
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (5, 5, "Hazy golden color with a thin white head that does not linger. Not too hoppy with hints of alkaloids on the nose. It has nice citrus hops with hints of grapefruit. This is a very enjoyable and drinkable IPA.", 4.45);
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (5, 5, 'Not as good as their imperial IPA (Fantastic Haze) - but rather enjoyable and excellent considering the reasonable price point for a case. Great value.', 4);
+
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (6, 1, 'Extremely good flavor, smell, and feel. This "palesner" is very good quality for a decent price. A hoppy taste, but not overbearing. A good choice for any occasion. Highly recommended.', 4.75);
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (6, 1, "Had this in a hotel lobby. Then had several. Best hoppy pils attempt that I've had. most are decent injections of bitterness during which your left brain thinks you're having a miller lite. This brew has nothin to hide.", 4.6);
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (6, 1, 'Looks super Thin with barely any head and yellow color. Barley and malty scent. Taste out does the look and smell, great barley malt toasted bread taste. Medium light body with medium carbonation. Suprisingly good!', 3.93);
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (6, 1, ' A nice beer. Not quite as hoppy as I expected given the mash-up style but it fits tie pils profile well & is very easy to drink', 3.84);
+
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (7, 2, 'Bottled. Light gold in color. Small head that sticks around. Faint floral smell. Thin on the pallet. Balanced well front to back. More bite on the back end. Nice beer', 3.84);
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (7, 2, 'Good whiff of piney and citrusy hops – in that order. There’s a bit of bready malt aroma way in the background.', 4.02);
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (7, 2, "Handsome amber brew with plenty of foam in the Dupont chalice. This is not as intensely bitter as typical craft IPAs. But it's also quite flavorful without being a fruit bomb. Aroma of lime peel and a Salty dog.", 3.73);
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (7, 2, "The taste is of citrus (especially grapefruit, some lemon) with piney/floral spicing. Grainy sweet backbone. Bitter, slightly medicinal finish. Alcohol well-integrated (giving just a hint of pepper, but not hot).", 3.9);
+
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (8, 2, "Stone brewing does a lot of IPA's so it is surprising to find another IPA in their line up. Not really. I find myself gravitating towards the scorpion bowl IPA yet this one is another good IPA from folks who know how to make an IPA.", 3.9);
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (8, 2, 'Non existent head, but nice deep color. Smells hoppy and a little sweet. Name is accurate, it is quite delicious. It’s got a great grain profile and hoppy complexity. Slightly bitter aftertaste but it’s not bad at all.', 3.7);
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (8, 2, 'I love gritty, dry IPAs with scrubbing carbonation like this. The alcohol again is a bit much.', 3.55);
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (8, 2, ' I really liked this beer a lot; especially nice in a reduced gluten offering. I can drink more before the bloating becomes the obvious.', 4.23);
+
+
+INSERT INTO reviews (beer_id, reviewer_id, review_text, star_rating) values (9, 2, 'No reviews yet!! ', 0);
+
+
