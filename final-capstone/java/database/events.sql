c89fb8920935097176486e6e59361094ac64becb
seror salman <serorsalman@gmail.com>
2021-05-31 15:55:46 -0500

    changed DB, added some news and events

--- final-capstone/java/database/events.sql
+++ final-capstone/java/database/events.sql
@@ -4,3 +4,16 @@
 
 INSERT INTO events (event_name, event_date, event_time, event_description, brewery_id)
 VALUES ('Nitro Coconut Tasting', '2021-06-05', '14:00:00', 'Come sample our newest brew! Ale with sea salt, dark chocolate, and natural chocolate & coconut flavors.', 1);
+
+INSERT INTO events (event_name, event_date, event_time, event_description, brewery_id)
+VALUES ('Blueberry Maple Stout Tasting', '2021-06-04', '16:00:00', 'Come sample our newest brew! Rich, sweet stout with classic malt characteristics with a bold, unique twist of blueberry and maple.', 1);
+
+INSERT INTO events (event_name, event_date, event_time, event_description, brewery_id)
+VALUES ('Ottermelon Hefeweizen Tasting', '2021-06-05', '16:00:00', 'Come sample our newest brew! This Pale Ale is heavily dosed with Mosaic hops.', 2);
+INSERT INTO events (event_name, event_date, event_time, event_description, brewery_id)
+VALUES ('Hazy Ipa Tasting', '2021-06-04', '17:00:00', 'Come sample our newest brew! A soft, rounded mouthfeel asserts itself in this unfiltered IPA, loaded with newfangled hop varieties.', 2);
+
+INSERT INTO events (event_name, event_date, event_time, event_description, brewery_id)
+VALUES ('Giant Pacific Octopus Tasting', '2021-06-11', '17:00:00', 'Come sample our newest brew! The taste is of citrus (especially grapefruit, some lemon) with piney/floral spicing.', 3);
+INSERT INTO events (event_name, event_date, event_time, event_description, brewery_id)
+VALUES ('Delicious IPA Tasting', '2021-06-12', '18:00:00', 'Come sample our newest brew! An IPA that lives up to the name.', 3);
--- final-capstone/java/database/news.sql
+++ final-capstone/java/database/news.sql
@@ -4,3 +4,10 @@
 
 INSERT INTO news (news_name, news_body, brewery_id)
 VALUES ('New Merch!', 'Come checkout all out our new merch and our redesigned brewshop. Open 10am-10pm daily.', 1);
+
+
+INSERT INTO news (news_name, news_body, brewery_id)
+VALUES ('Beers with the Brewer!', 'Come checkout all out our new and redesigned brewshop. Open 10am-10pm daily.', 2);
+
+INSERT INTO news (news_name, news_body, brewery_id)
+VALUES ('Belgian-style beer!', "A new microbrewery that specializes in Belgian-style beers is opening in Detroit's Islandview neighborhood. 5pm-8pm ", 3);
