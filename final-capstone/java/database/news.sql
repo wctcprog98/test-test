2aca40ba2b41cca4f0d05add7078dcd3e7a7c89d
seror salman <serorsalman@gmail.com>
2021-05-31 16:16:46 -0500

    changed DB, added some news and events and some fix

--- final-capstone/java/database/news.sql
+++ final-capstone/java/database/news.sql
@@ -10,4 +10,4 @@ INSERT INTO news (news_name, news_body, brewery_id)
 VALUES ('Beers with the Brewer!', 'Come checkout all out our new and redesigned brewshop. Open 10am-10pm daily.', 2);
 
 INSERT INTO news (news_name, news_body, brewery_id)
-VALUES ('Belgian-style beer!', "A new microbrewery that specializes in Belgian-style beers is opening in Detroit's Islandview neighborhood. 5pm-8pm ", 3);
+VALUES ('Belgian-style beer!', 'A new microbrewery that specializes in Belgian-style beers is opening in Detroits Islandview neighborhood. 5pm-8pm ', 3);
