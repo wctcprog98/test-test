d19b837d872577c5e7a6c0eac282516759ed8827
Nenye Diei <nenyediei@gmail.com>
2021-06-01 16:41:21 -0500

    adding changes to test

--- final-capstone/vue/src/components/EventsList.vue
+++ final-capstone/vue/src/components/EventsList.vue
@@ -10,7 +10,7 @@
       {{ event.eventName }} <br />
       {{ event.eventDate | formatDate }}
       <br />
-      {{ event.eventTime }} <br />
+      {{ formatTime(event.eventTime) }} <br />
       {{ event.eventName }}
       {{ event.eventDescription }}
     </p>
@@ -26,9 +26,28 @@ export default {
     };
   },
   created() {
-    EventService.listByBreweryId(1).then((response) => {
+    EventService.listUpComing().then((response) => {
       this.allEvents = response.data;
     });
+  },
+  methods: {
+    formatTime(time) {
+      console.log(time.substring(0, 2));
+      let hour = time.substring(0, 2);
+      const min = time.substring(2, 5);
+      let period = " AM";
+
+      if (hour == 12) {
+        period = " PM";
+      } else if (hour == 0) {
+        hour = 12;
+      } else if (hour > 12) {
+        hour = hour % 12;
+        period = " PM";
+      }
+
+      return hour + min + period;
+    },
   },
 };
 </script>
