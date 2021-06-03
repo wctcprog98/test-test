775682f44c78c58bb711751c7bff70cb0716fb37
Jon Camerer <jon.camerer@gmail.com>
2021-06-03 12:15:10 -0500

    Added header to registration page

---
+++ final-capstone/vue/src/components/RegistrationHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Header">
+    <div class="flex-container">
+      <router-link v-bind:to="{ name: 'home' }">
+        <div>
+          <img src="../images/logo.png" />
+        </div>
+      </router-link>
+      <div class="title">Brewery Finder</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- final-capstone/vue/src/views/Register.vue
+++ final-capstone/vue/src/views/Register.vue
@@ -1,62 +1,67 @@
 <template>
-  <div id="register" class="text-center">
-    <form class="form-register" @submit.prevent="register">
-      <h1 class="h3 mb-3 font-weight-normal">Create Account</h1>
-      <div class="alert alert-danger" role="alert" v-if="registrationErrors">
-        {{ registrationErrorMsg }}
-      </div>
-      <label for="username" class="sr-only">Username</label>
-      <input
-        type="text"
-        id="username"
-        class="form-control"
-        placeholder="Username"
-        v-model="user.username"
-        required
-        autofocus
-      />
-      <label for="password" class="sr-only">Password</label>
-      <input
-        type="password"
-        id="password"
-        class="form-control"
-        placeholder="Password"
-        v-model="user.password"
-        required
-      />
-      <input
-        type="password"
-        id="confirmPassword"
-        class="form-control"
-        placeholder="Confirm Password"
-        v-model="user.confirmPassword"
-        required
-      />
-      <label for="account-type">Account Type</label>
-      <select
-        name="account-type"
-        class="form-control"
-        v-model="user.accountType"
-      >
-        <option value="">--Please select a role</option>
-        <option value="Beer Lover">Beer Lover</option>
-        <option value="Brewer">Brewer</option>
-        <option value="Administrator">Administrator</option>
-      </select>
-      <label for="user-location">Location:</label>
-      <input name="user-location" type="text" v-model="user.userLocation" />
-      <router-link :to="{ name: 'home' }">Have an account?</router-link>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">
-        Create Account
-      </button>
-    </form>
+  <div>
+    <registration-header class="reg-header" />
+    <div id="register" class="text-center">
+      <form class="form-register" @submit.prevent="register">
+        <h1 class="h3 mb-3 font-weight-normal">Create Account</h1>
+        <div class="alert alert-danger" role="alert" v-if="registrationErrors">
+          {{ registrationErrorMsg }}
+        </div>
+        <label for="username" class="sr-only">Username</label>
+        <input
+          type="text"
+          id="username"
+          class="form-control"
+          placeholder="Username"
+          v-model="user.username"
+          required
+          autofocus
+        />
+        <label for="password" class="sr-only">Password</label>
+        <input
+          type="password"
+          id="password"
+          class="form-control"
+          placeholder="Password"
+          v-model="user.password"
+          required
+        />
+        <input
+          type="password"
+          id="confirmPassword"
+          class="form-control"
+          placeholder="Confirm Password"
+          v-model="user.confirmPassword"
+          required
+        />
+        <label for="account-type">Account Type</label>
+        <select
+          name="account-type"
+          class="form-control"
+          v-model="user.accountType"
+        >
+          <option value="">--Please select a role</option>
+          <option value="Beer Lover">Beer Lover</option>
+          <option value="Brewer">Brewer</option>
+          <option value="Administrator">Administrator</option>
+        </select>
+        <label for="user-location">Location:</label>
+        <input name="user-location" type="text" v-model="user.userLocation" />
+        <router-link :to="{ name: 'home' }">Have an account?</router-link>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">
+          Create Account
+        </button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
+import RegistrationHeader from "../components/RegistrationHeader.vue";
 import authService from "../services/AuthService";
 
 export default {
+  components: { RegistrationHeader },
   name: "register",
   data() {
     return {
